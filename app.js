f97231ff2e1576eab2a714659513805fc4261a7e
Steven Jackson <steve@sjackson.net>
2017-12-11 15:30:39 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,33 @@
+//app.js
+App({
+  onLaunch: function () {
+    var logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+
+    // 登录
+    wx.login({
+      success: res => {
+      }
+    })
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          wx.getUserInfo({
+            success: res => {
+              this.globalData.user
+              fo = res.userInfo
+
+              if (this.userInfoReadyCallback) {
+                this.userInfoReadyCallback(res)
+              }
+            }
+          })
+        }
+      }
+    })
+  },
+  globalData: {
+    userInfo: null
+  }
+})
---
+++ pages/top_stories/top_stories.js
@@ -0,0 +1,87 @@
+// pages/top_stories/top_stories.js
+Page({
+  data: {    
+    stories: [
+      {
+        id: 1,
+        title: "Introduction to logic programming with Prolog",
+        url: "www.matchilling.com",
+        posted_at: "1h",
+        comment_count: 19,
+        upvote_count: 55
+      },
+      {
+        id: 2,
+        title: "The compelling case for working less",
+        url: "www.bbc.com",
+        posted_at: "9h",
+        comment_count: 120,
+        upvote_count: 215
+      },
+      {
+        id: 1,
+        title: "Mastering Chess and Shogi by Self-Play with General Reinforcement learning",
+        url: "www.arxiv.org",
+        posted_at: "3 hours ago",
+        comment_count: 48,
+        upvote_count: 171
+      },
+      {
+        id: 1,
+        title: "I don't like your examples",
+        url: "www.oreilly.com",
+        posted_at: "1h",
+        comment_count: 19,
+        upvote_count: 24
+      },
+      {
+        id: 1,
+        title: "Portual's radical drugs policy is working. Why hasn't the world copied it?",
+        url: "www.portugal.com",
+        posted_at: "1h",
+        comment_count: 20,
+        upvote_count: 1042
+      },
+      {
+        id: 1,
+        title: "Mozilla Files Cross-Complaint against Yahoo Yholdings and Oath",
+        url: "www.mozilla.com",
+        posted_at: "1h",
+        comment_count: 19,
+        upvote_count: 55
+      },
+      {
+        id: 1,
+        title: "Neural Networks in Javascript with Deeplearn.js",
+        url: "www.neuralnetworks.com",
+        posted_at: "1h",
+        comment_count: 0,
+        upvote_count: 408
+      },
+      {
+        id: 1,
+        title: "Parcel - A fast, zero configuration web bundler",
+        url: "www.parcel.com",
+        posted_at: "14h ago",
+        comment_count: 19,
+        upvote_count: 592
+      },
+      {
+        id: 1,
+        title: "Cinema's Greatest Scene: 'Casablanca' and 'La Marseillaise'",
+        url: "www.cinema.com",
+        posted_at: "14h ago",
+        comment_count: 19,
+        upvote_count: 592
+      },
+      {
+        id: 1,
+        title: "The FastMail Security Mindset",
+        url: "www.fastmail.com",
+        posted_at: "14h ago",
+        comment_count: 19,
+        upvote_count: 592
+      }
+    ]
+  },
+})
